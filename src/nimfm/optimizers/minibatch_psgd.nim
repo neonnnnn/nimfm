1b38ff791a2258f13f592cc9b419f2eff81d4010
neonnnnn <katarashi@ist.hokudai.ac.jp>
2020-10-16 00:28:00 +0900

    Add PGD/PSGD/PCD/PBCD-based solvers for sparse regularization

---
+++ src/nimfm/optimizers/minibatch_psgd.nim
@@ -0,0 +1,213 @@
+import ../dataset, ../tensor/tensor
+import ../models/factorization_machine, ../models/fm_base, ../models/params
+import optimizer_base, utils
+from ../loss import newSquared
+from ../regularizers/regularizers import newSquaredL12
+from sgd import predictWithGrad, getEta, SchedulingKind
+from pgd import finalize
+import math, sugar, sequtils, random, algorithm
+
+
+type
+  MBPSGD*[L, R] = ref object of BaseCSROptimizer
+    gamma*: float64
+    eta0: float64
+    loss*: L
+    reg*: R
+    scheduling: SchedulingKind
+    power: float64
+    miniBatchSize: int
+    maxIterInner: int
+    shuffle: bool
+    it: int
+    nCalls: int
+
+
+proc newMBPSGD*[L, R](maxIter=100, eta0=0.1, alpha0=1e-6, alpha=1e-3,
+                      beta=1e-4, gamma=1e-4, loss: L=newSquared(),
+                      reg: R=newSquaredL12(), miniBatchSize = -1,
+                      maxIterInner = -1, scheduling = optimal,
+                      power = 1.0, verbose = 1, tol = 1e-6,
+                      shuffle=true, nCalls = -1): MBPSGD[L, R] =
+  ## Creates new mini-batch proximal stochastic variance reduction
+  ## gradient (MBPSGD).
+  ## maxIter: Maximum number of iteration. At each iteration, all parameters
+  ##          are updated maxIterInner times.
+  ## eta0: step-size.
+  ## alpha0: Regularization-strength for intercept.
+  ## alpha: Regularization-strength for linear term.
+  ## beta: Regularization-strength for higher-order weights.
+  ## gamma: Sparsity-inducing-regularization-strength for higher-order weights.
+  ## loss: Loss function. It must have mu: float64 field and
+  ##       loss/dloss proc: (float64, float64)->float64.
+  ## reg: Sparsity-inducing regularization.
+  ## miniBatchSize: Number of samples in one minibatch. If <= 0, it is set to be 
+  ##                minibatchsize = nFeatures * nSamples / nnz(X).
+  ## maxIterInner: Maximum number of inner iteration (= the number of used
+  ##               minibatches). If <= 0, it is set to be
+  ##               maxIterInner = nSamples / miniBatchSize.
+  ## scheduling: How to change the step-size.
+  ##  - constant: eta = eta0,
+  ##  - optimal: eta = eta0 / pow(1+eta0*regul*it, power),
+  ##  - invscaling: eta = eta0 / pow(it, power),
+  ##  - pegasos: eta = 1.0 / (regul * it),
+  ##  where regul is the Regularization-strength hyperparameter.
+  ## power: Hyperparameter for step size scheduling.
+  ## shuffle: How to choose one instance: cyclic (false) or random permutation
+  ##          (true).
+  ## verbose: Whether to print information on optimization processes.
+  ## tol: Tolerance hyperparameter for stopping criterion.
+  
+  result = MBPSGD[L, R](
+    maxIter: maxIter, alpha0: alpha0, alpha: alpha, beta: beta, gamma: gamma,
+    loss: loss, reg: reg, eta0: eta0, tol: tol, verbose: verbose, it: 0,
+    scheduling: scheduling, power: power, miniBatchSize: miniBatchSize,
+    maxIterInner: maxIterInner, shuffle: shuffle)
+
+
+proc updateGradient*[L](X: RowDataset, y: seq[float64], i: int,
+                        params, grads: Params, A: var Matrix, dA: var Tensor,
+                        loss: L, degree, nAugments, miniBatchSize: int,
+                        sign: float64): float64  =
+  let yPred  = predictWithGrad(X, i, params.P, params.w, params.intercept, 
+                               A, dA, degree, nAugments)
+  result = loss.loss(y[i], yPred)
+  let coef = sign * loss.dloss(y[i], yPred) / float(miniBatchSize)
+
+  X.addDummyFeature(1.0, nAugments)
+  for order in 0..<params.P.shape[0]:
+    for j in X.getRowIndices(i):
+      for s in 0..<params.P.shape[2]:
+        grads.P[order, j, s] += coef * dA[order, j, s]
+  X.removeDummyFeature(nAugments)
+
+  if params.fitLinear:
+    for (j, val) in X.getRow(i):
+      grads.w[j] += coef * val
+  
+  if params.fitIntercept:
+    grads.intercept += coef
+
+
+proc epoch[L, R](self: MBPSGD[L, R], X: RowDataset, y: seq[float64],
+                 params, grads: Params, A: var Matrix, dA: var Tensor,
+                 indices: var seq[int], degree, nAugments: int,
+                 miniBatchSize, maxIterInner: int, ii: var int): float64 =
+  result = 0.0
+  for itInner in 0..<maxIterInner:
+    # compute gradient among a minibatch
+    var b: int = 0
+    grads <- 0.0
+
+    # sample a minibatch
+    while b < miniBatchSize:
+      let i = indices[ii]
+      result += updateGradient(X, y, i, params, grads, A, dA, self.loss,
+                               degree, nAugments, miniBatchSize, 1.0)
+      inc(b)
+      inc(ii)
+      if ii >= X.nSamples:
+        ii = 0
+        if self.shuffle:
+          shuffle(indices)
+    
+    # gradient update
+    let eta_P = self.getEta(self.beta)
+    let eta_w = self.getEta(self.alpha)
+    let eta_intercept = self.getEta(self.alpha0)
+    params.step(grads, eta_intercept, eta_w, eta_P, self.alpha0,
+                self.alpha, self.beta)
+    for order in 0..<params.P.shape[0]:
+      self.reg.prox(params.P[order], self.gamma * eta_P / (1.0 + eta_P*self.beta),
+                    degree-order)
+    inc(self.it)
+
+  result /= float(miniBatchSize * maxIterInner)
+
+
+proc fit*[L, R](self: MBPSGD[L, R], X: RowDataset, y: seq[float64],
+                sfm: FactorizationMachine,
+                callback: (MBPSGD[L, R], FactorizationMachine)->void = nil) =
+  ## Fits the sparse factorization machine on X and y by accelerated pgd.
+  sfm.init(X)
+  let y = sfm.checkTarget(y)
+  let
+    nSamples = X.nSamples
+    nFeatures = X.nFeatures
+    nComponents = sfm.P.shape[1]
+    nOrders = sfm.P.shape[0]
+    degree = sfm.degree
+    fitLinear = sfm.fitLinear
+    fitIntercept = sfm.fitIntercept
+    nAugments = sfm.nAugments
+  var
+    dL: Vector = zeros([nSamples])
+    P: Tensor = zeros([nOrders, sfm.P.shape[2], nComponents])
+    params = newParams(P, sfm.w, sfm.intercept, fitLinear, fitIntercept)
+    A: Matrix = zeros([nComponents, degree+1])
+    dA: Tensor  = zeros(P.shape)
+    grads = newParams(P.shape, sfm.w.len, fitLinear, fitIntercept)
+    indices = toSeq(0..<nSamples)
+    isConverged = false
+
+  if not sfm.warmstart:
+    self.it = 1
+
+  # copy for fast training
+  for order in 0..<sfm.P.shape[0]:
+    P[order] = sfm.P[order].T
+
+  var miniBatchSize = self.miniBatchSize
+  if miniBatchSize <= 0:
+    miniBatchSize = (nFeatures * nSamples) div X.nnz
+    miniBatchSize = max(miniBatchSize, 1)
+  var maxIterInner = self.maxIterInner
+  if maxIterInner <= 0:
+    maxIterInner = (nSamples-1) div miniBatchSize + 1
+    maxIterInner = max(maxIterinner, 1)
+
+  # perform optimization
+  var ii = 0
+  if self.shuffle:
+    shuffle(indices)
+    
+  self.reg.initSGD(degree, nFeatures+nAugments, nComponents)
+  
+  if self.verbose > 0: # echo header
+    echo("Minibatch size: ", miniBatchSize)
+    echo("Number of inner iteration: ", maxIterInner)
+    echoHeader(self.maxIter, viol=false)
+  
+  var old_lossVal = Inf
+  for it in 0..<self.maxIter:
+    # perform inner loop
+    let runningLoss = epoch(self, X, y, params, grads, A, dA, indices, degree,
+                            nAugments, miniBatchSize, maxIterInner, ii)
+    
+    if not callback.isNil:
+      finalize(sfm, params)
+      callback(self, sfm)  
+    
+    if runningLoss.classify == fcNan:
+      echo("Loss is NaN. Use smaller learning rate.")
+      break
+
+    if self.verbose > 0:
+      var regVal = regularization(P, params.w, params.intercept, self.alpha0,
+                                  self.alpha, self.beta)
+      for order in 0..<nOrders:
+        regVal += self.gamma * self.reg.eval(P[order], sfm.degree-order)
+      echoInfo(it+1, self.maxIter, -1, runningLoss, regVal)
+    
+    if abs(old_lossVal - runningLoss) < self.tol:
+      if self.verbose > 0: echo("Converged at epoch ", it+1, ".")
+      isConverged = true
+      break
+
+    old_lossVal = runningLoss
+
+  if not isConverged and self.verbose > 0:
+    echo("Objective did not converge. Increase maxIter.")
+
+  # finalize
+  finalize(sfm, params)
---
+++ src/nimfm/optimizers/pbcd.nim
@@ -0,0 +1,340 @@
+import ../dataset, ../tensor/tensor, ../kernels, ../models/factorization_machine
+import ../models/fm_base, ../extmath
+from ../loss import newSquared
+from ../regularizers/regularizers import newSquaredL21
+import fit_linear, optimizer_base, utils
+import sequtils, math, random, sugar
+
+type
+  PBCD*[L, R] = ref object of BaseCSCOptimizer
+    ## Proximal block coordinate descent solver.
+    gamma*: float64
+    loss*: L
+    reg*: R
+    sigma: float64
+    rho: float64
+    maxSearch: int
+    shrink: bool
+    shuffle: bool
+
+
+proc newPBCD*[L, R](maxIter = 100, alpha0=1e-6, alpha=1e-3, beta=1e-4, 
+                    gamma=1e-4, loss: L=newSquared(), reg: R=newSquaredL21(),
+                    verbose = 1, tol = 1e-3, sigma=0.01, rho=0.5, maxSearch=0,
+                    shrink=false, shuffle=false): PBCD[L, R] =
+  ## Creates new PBCD.
+  ## maxIter: Maximum number of iteration. At each iteration,
+  ##          all parameters are updated once by using all samples.
+  ## alpha0: Regularization-strength for intercept.
+  ## alpha: Regularization-strength for linear term.
+  ## beta: Regularization-strength for higher-order weights.
+  ## gamma: Sparsity-inducing-regularization-strength for higher-order weights.
+  ## loss: Loss function. It must have mu: float64 field and
+  ##       loss/dloss proc: (float64, float64)->float64.
+  ## reg: Sparsity-inducing regularization.
+  ## verbose: Whether to print information on optimization processes.
+  ## tol: Tolerance hyperparameter for stopping criterion.
+  ## rho: Paraneter for line search.
+  ## sigma: Parameter for line search.
+  ## maxSearch: Maximum number of iteration in line search for the step-size.
+  ##            If 0, algorithm uses the Lipschitz constant of the gradient.
+  ## shrink: Whether to shrink or not. If true, algorithm do not perform line search
+  ##         when row vector is updated as zero vector.
+  ## shuffle: Whether cyclic (false) or random permutation (true) order.
+  result = PBCD[L, R](
+    maxIter: maxIter, alpha0: alpha0, alpha: alpha, beta: beta, gamma: gamma,
+    loss: loss, reg: reg, tol: tol, verbose: verbose, sigma: sigma, rho:rho,
+    maxSearch: maxSearch, shrink: shrink, shuffle:shuffle)
+
+
+proc perm(x: var seq[int]) =
+  let n = len(x)
+  for i in 0..<len(x):
+    let pivot = i+rand(n-1-i)
+    let tmp = x[i]
+    x[i] = x[pivot]
+    x[pivot] = tmp
+
+
+proc computeDerivative*(X: ColDataset, pj: Vector, A, dA: var Tensor,
+                        j, degree: int) {.inline.} =
+  let nComponents = dA.shape[2]
+  for (i, val) in X.getCol(j):
+    for s in 0..<nComponents:
+      dA[0, i, s] = val
+  for deg in 1..<degree:
+    for (i, val) in X.getCol(j):
+      for s in 0..<nComponents:
+        dA[deg, i, s] = val * (A[deg, i, s] - pj[s] * dA[deg-1, i, s])
+  
+
+proc precomputeAnova(X: ColDataset, P: Matrix, A: var Tensor, degree: int) =
+  ## Computes ANOVA kernels between all samples and all base vectors.
+  let
+    nSamples = X.nSamples
+    nFeatures = X.nFeatures
+    nComponents = P.shape[1]
+  
+  for t in 1..<degree+1:
+    for i in 0..<nSamples:
+      for s in 0..<nComponents:
+        A[t, i, s] = 0
+
+  for j in 0..<nFeatures:
+    for t in 0..<degree:
+      for (i, val) in X.getCol(j):
+        for s in 0..<nComponents:
+          A[degree-t, i, s] += A[degree-t-1, i, s] * val * P[j, s]
+
+
+proc lineSearch[L, R](self: PBCD[L, R], X: ColDataset, y: seq[float64], 
+                      yPred: var seq[float64], P: var Matrix, degree, j: int,
+                      A, dA: var Tensor, delta: var Vector, cond:float64,
+                      newLossVal: var float64, oldLossVal,
+                      oldRegVal: float64): float64 =
+  let nComponents = P.shape[1]
+  let nSamples = float(X.nSamples)
+  var alpha = 1.0
+  var newRegVal = self.gamma*self.reg.value + 0.5*self.beta*norm(P[j], 2)^2
+  var decreasing = (newLossVal / nSamples) + newRegVal
+  decreasing -= (oldLossVal / nSamples) + oldRegVal
+
+  var it = 0
+  while not (decreasing <= self.sigma * alpha * cond):
+    if it >= self.maxSearch: break
+    for s in 0..<nComponents:
+      P[j, s] -= alpha * (self.rho - 1) * delta[s]
+    for (i, val) in X.getCol(j):
+      newLossVal -= self.loss.loss(y[i], yPred[i])
+      yPred[i] -= dot(dA[degree-1, i], delta) * alpha * (self.rho-1)
+      newLossVal += self.loss.loss(y[i], yPred[i])
+
+    alpha *= self.rho
+    self.reg.updateCacheBCD(P, degree, j)
+    newRegVal = 0.5*self.beta*norm(P[j], 2)^2 + self.gamma*self.reg.value
+    decreasing = (newLossVal / nSamples) + newRegVal
+    decreasing -= (oldLossVal / nSamples) + oldRegVal
+    inc(it)
+  delta *= alpha
+  result = norm(delta, 1)
+
+
+proc update[L, R](self: PBCD[L, R], P: var Matrix, X: ColDataset,
+                  y: seq[float64], yPred: seq[float64], degree, j: int,
+                  A, dA: var Tensor, grad, invStepSizes, delta: var Vector) =
+  let
+    nComponents = P.shape[1]
+    nSamples = X.nSamples
+
+  for s in 0..<nComponents:
+    grad[s] = 0
+    invStepSizes[s] = 0
+
+  # computes grad and invStepSizes for all s \in [nComponents]
+  if degree > 2: computeDerivative(X, P[j], A, dA, j, degree)
+  else:
+    for (i, val) in X.getCol(j):
+      for s in 0..<nComponents:
+        dA[1, i, s] = (val * (A[1, i, s] - val * P[j, s]))
+
+  for (i, val) in X.getCol(j):
+    let dL =  self.loss.dloss(y[i], yPred[i])
+    for s in 0..<nComponents:
+      grad[s] += dL * dA[degree-1, i, s]
+      invStepSizes[s] += dA[degree-1, i, s]^2
+
+  grad /= float(nSamples)
+  for s in 0..<nComponents:
+    grad[s] += self.beta * P[j, s]
+  var invStepSize: float64
+  # line search
+  if self.maxSearch != 0:
+    invStepSizes *= self.loss.mu / float(nSamples)
+    invStepSize = max(invStepSizes)
+  else: # without line search
+    invStepSize = sum(invStepSizes) * self.loss.mu / float(nSamples)
+
+  invStepSize += self.beta
+  invStepSize = max(invStepSize, 1e-12)
+  # updates
+  for s in 0..<nComponents:
+    delta[s] = P[j, s] # old value
+    P[j, s] -= grad[s] / invStepSize
+  
+  self.reg.prox(P[j], self.gamma/invStepSize, degree, j)
+  self.reg.updateCacheBCD(P, degree, j)
+  for s in 0..<nComponents:
+    delta[s] = - P[j, s] + delta[s] # delta = -new + old
+
+
+proc epoch[L, R](self: PBCD[L, R], X: ColDataset, y: seq[float64],
+                 yPred: var seq[float64], P: var Matrix,
+                 degree: int, A, dA: var Tensor,
+                 grad, invStepSizes, delta: var Vector,
+                 lossVal: var float64): float64 =
+  result = 0.0
+  let 
+    nFeatures = X.nFeatures
+    nComponents = A.shape[2]
+  var
+    newLossVal, oldLossVal, oldRegVal, cond: float64
+    indices = toSeq(0..<nFeatures)
+  if self.shuffle:
+    perm(indices)
+
+  oldLossVal = lossVal
+  self.reg.computeCacheBCD(P, degree, indices)
+  for j in indices:
+    newLossVal = oldLossVal
+    oldRegVal = 0.5*self.beta*norm(P[j], 2)^2 + self.gamma*self.reg.value
+    cond = - self.gamma * self.reg.value
+
+    update(self, P, X, y, yPred, degree, j, A, dA, grad, invStepSizes, delta)
+
+    # synchronize yPred
+    for (i, val) in X.getCol(j):
+      newLossVal -= self.loss.loss(y[i], yPred[i])
+      yPred[i] -= dot(delta, dA[degree-1, i])
+      newLossVal += self.loss.loss(y[i], yPred[i])
+
+    # without line search
+    if self.maxSearch == 0:
+      result += norm(delta, 1)
+    else: # perform line search
+      cond += self.gamma * self.reg.value
+      cond -= dot(grad, delta)
+      result += lineSearch(self, X, y, yPred, P, degree, j,  A, dA, delta,
+                           cond, newLossVal, oldLossVal, oldRegVal)
+    # synchronize A
+    if degree == 2:
+      for (i, val) in X.getCol(j):
+        for s in 0..<nComponents:
+          A[1, i, s] -= val * delta[s]
+    else:
+      for deg in 1..<degree:
+        for (i, val) in X.getCol(j):
+          for s in 0..<nComponents:
+            A[deg, i, s] -= dA[deg-1, i, s] * delta[s]
+    oldLossVal = newLossVal
+
+  lossVal = newLossVal
+
+
+proc fit*[L, R](self: PBCD[L, R], X: ColDataset, y: seq[float64],
+                sfm: FactorizationMachine,
+                callback: (self: PBCD[L, R], FactorizationMachine)->void = nil) =
+  ## Fits the factorization machine on X and y by coordinate descent.
+  ## Update pj \in R^d at each iteration.
+  sfm.init(X)
+
+  let y = sfm.checkTarget(y)
+  let
+    nSamples = X.nSamples
+    nFeatures = X.nFeatures
+    nComponents = sfm.P.shape[1]
+    nOrders = sfm.P.shape[0]
+    degree = sfm.degree
+    alpha0 = self.alpha0 * float(nSamples)
+    alpha = self.alpha * float(nSamples)
+    fitLinear = sfm.fitLinear
+    fitIntercept = sfm.fitIntercept
+    nAugments = sfm.nAugments
+
+  # caches
+  var
+    yPred = newSeqWith(nSamples, 0.0)
+    A: Tensor = zeros([degree+1, nSamples, nComponents])
+    dA: Tensor = zeros([degree, nSamples, nComponents])
+    grad: Vector = zeros([nComponents])
+    delta: Vector = zeros([nComponents])
+    invStepSizes: Vector = zeros([nComponents])
+    colNormSq: Vector
+    isConverged = false
+    lossVal, regVal: float64
+    P: Tensor = zeros([nOrders, nFeatures+nAugments, nComponents])
+  
+  # for fast training
+  for order in 0..<nOrders:
+    for j in 0..<nFeatures+nAugments:
+      for s in 0..<nComponents:
+        P[order, j, s] = sfm.P[order, s, j]
+  
+  # init caches
+  for i in 0..<nSamples:
+    for s in 0..<nComponents:
+      A[0, i, s] = 1.0
+
+  if fitLinear:
+    colNormSq = norm(X, axis=0, p=2)
+    colNormSq *= colNormSq
+
+  # compute prediction
+  linear(X, sfm.w, yPred)
+  yPred += sfm.intercept
+
+  X.addDummyFeature(1.0, nAugments)
+  for order in 0..<nOrders:
+    precomputeAnova(X, P[order], A, degree-order)
+    for i in 0..<nSamples:
+      for s in 0..<nComponents:
+        yPred[i] += A[degree-order, i, s]
+  
+  self.reg.initBCD(degree, nFeatures+nAugments, nComponents)
+  if self.verbose > 0: # echo header
+    echoHeader(self.maxIter)
+
+  for it in 0..<self.maxIter:
+    var viol = 0.0
+
+    X.removeDummyFeature(nAugments)
+    if fitIntercept:
+      viol += fitInterceptCD(sfm.intercept, y, yPred, nSamples, alpha0,
+                             self.loss)
+
+    if fitLinear:
+      viol += fitLinearCD(sfm.w, X, y, yPred, colNormSq, alpha, self.loss)
+    
+    # compute loss value
+    lossVal = 0.0
+    for i in 0..<nSamples:
+      lossVal += self.loss.loss(y[i], yPred[i])
+    
+    X.addDummyFeature(1.0, nAugments)
+    if nOrders == 1:
+      viol += epoch(self, X, y, yPred, P[0], degree, A, dA, grad,
+                    invStepSizes, delta, lossVal)
+    else:
+      for order in 0..<nOrders:
+        precomputeAnova(X, P[order], A, degree-order)
+        viol += epoch(self, X, y, yPred, P[order], degree-order, A, dA, grad,
+                      invStepSizes, delta, lossVal)
+
+    lossVal /= float(nSamples)
+    if self.verbose > 0:
+      regVal = regularization(P, sfm.w, sfm.intercept, self.alpha0, self.alpha,
+                              self.beta)
+      for order in 0..<nOrders:
+        regVal += self.gamma * self.reg.eval(P[order], degree-order)
+      echoInfo(it+1, self.maxIter, viol, lossVal, regVal)
+
+    if not callback.isNil:
+      for order in 0..<nOrders:
+        for j in 0..<nFeatures+nAugments:
+          for s in 0..<nComponents:
+            sfm.P[order, s, j] = P[order, j, s]
+      callback(self, sfm)
+
+    if viol < self.tol:
+      if self.verbose > 0: 
+        echo("Converged at iteration ", it+1, ".")
+      isConverged = true
+      break
+
+  X.removeDummyFeature(nAugments)
+  # finalize
+  for order in 0..<nOrders:
+    for j in 0..<nFeatures+nAugments:
+      for s in 0..<nComponents:
+        sfm.P[order, s, j] = P[order, j, s]
+  if not isConverged and self.verbose > 0:
+    echo("Objective did not converge. Increase maxIter.")
